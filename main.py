2eb0aa36419c1c07fb9b80bd081445c594de8f12
orlandbertblick-gif <orlandbertblick@gmail.com>
2026-06-17 16:21:47 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ ADMIN_ID = 8672817508                # الـ ID بتاعك كمدير للبو�
 
 # 🔑 بيانات الحسابات لموقع Durian 
 DURIAN_ACCOUNTS = [
-    ["Abdelhadi2005", "OXgwaDJnNXIraDByNEVxRXFsNWVEUT09"],
+    ["3bdelhadisayed", "N3BIVTV2OWxheFFYenpFL0NrbW45Zz09"],
     ["Abdelhadi2005", "OXgwaDJnNXIraDByNEVxRXFsNWVEUT09"]
 ]
 
